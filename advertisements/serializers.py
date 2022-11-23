7d3b3c4f0ff77b40ced128ed243a3ea2a80bea54
Tryd0g0lik <work80@mail.ru>
2022-11-23 10:11:37 +0700

    beginning works over the status filter

--- advertisements/serializers.py
+++ advertisements/serializers.py
@@ -110,5 +110,7 @@ class AdvertisementSerializer(serializers.ModelSerializer):
             instance.title = validated_data.get('title', instance.title)
             instance.description = validated_data.get('description', instance.description)
             instance.save()
-
             return instance
+
+
+
